13a34bbf57c36e05c1af658baae16c88b4d572d9
Sawada Hideshige <hideshige.sawada@digitalhearts.com>
2017-07-07 13:40:38 +0900

    mod view

--- device/view.php
+++ device/view.php
@@ -14,8 +14,10 @@
  *
  * <!-- ELEMENT *** -->には指定のテンプレートが挿入される。
  * 
+ * 携帯の場合、$_SESSION['mobile_pc_flag']で携帯とPCの表示切り替えができる
+ * 
  * @author   Sawada Hideshige
- * @version  1.1.5.0
+ * @version  1.1.6.0
  * @package  device
  * 
  */
@@ -30,27 +32,16 @@ class View
      * @param array $disp テンプレートに埋め込むデータ配列
      * @return string
      */
-    public static function template(
-        string $tpl,
-        array $disp
-    ): string {
+    public static function template(string $tpl, array $disp): string
+    {
         try {
-            $content = self::open($tpl);
-
-            //エレメントの反映
-            $content = self::elementMatch($content);
-            $content = str_replace("\n", '=br=', $content);
-
-            //コンテンツの下準備のため先に変換しておく
-            if (isset ($disp['REPLACE'])) {
-                foreach ($disp['REPLACE'] as $k => $v) {
-                    $content = str_replace('{' . $k . '}', $v, $content);
-                }
-                unset($disp['REPLACE']);
+            $content = '';
+            $open = self::open($tpl);
+            if ($open) {
+                $content = self::elementMatch($open);
+                $content = self::replace($disp, $content);
+                $content = self::match($disp, $content);
             }
-
-            $content = self::match($disp, $content);
-            $content = str_replace('=br=', "\n", $content);
         } catch (\Error $e) {
             $info = new ErrorInfo;
             $info->set($e->getMessage(), $e->getFile(), $e->getLine());
@@ -61,93 +52,13 @@ class View
     }
 
     /**
-     * エレメント部の処理
-     * @param string $content
-     * @return string
-     */
-    private static function elementMatch(string $content): string
-    {
-        //部分テンプレートの挿入
-        $element_match = [];
-        preg_match_all('/<!-- ELEMENT (.*?) -->/', $content, $element_match);
-
-        if ($element_match[1]) {
-            $element_match[1] = array_unique($element_match[1]);
-            foreach ($element_match[1] as $ek => $element_name) {
-                $element = str_replace("\n", '=br=', self::open($element_name));
-                $content = str_replace($element_match[0][$ek],
-                    $element, $content);
-            }
-            $content = self::elementMatch($content);
-        }
-        return $content;
-    }
-
-
-    /**
-     * テンプレートにパラメーターを挿入
-     * @param array|string $disp テンプレートに埋め込むデータ
-     * @param string $content テンプレート
-     * @return string 挿入し終えたコンテンツ
-     */
-    private static function match($disp, string $content): string
-    {
-        $match = [];
-        preg_match_all('/<!-- BEGIN (.*?) -->/', $content, $match);
-
-        foreach ($match[1] as $name) {
-            $pattern = '/<!-- BEGIN ' . $name
-                . ' -->(.*)<!-- END ' . $name . ' -->/';
-            preg_match_all($pattern, $content, $match1);
-            $tag_data = $original = isset($match1[1][0]) ? $match1[1][0] : '';
-            $all_tag = '';
-
-            preg_match_all('/{(.*?)}/', $original, $match2);
-
-            if (isset($disp[$name])) {
-                $num = count($disp[$name]);
-                for ($i = 0; $i < $num; $i ++) {
-                    $tag_data = isset($disp[$name][$i]) ?
-                        self::match($disp[$name][$i], $tag_data) : $tag_data;
-                    foreach ($match2[1] as $data) {
-                        if (isset($disp[$name][$i][$data])) {
-                            $change_data = $disp[$name][$i][$data];
-                        } else if (preg_match('/[ ,:;=]/', $data)) {
-                            //改行やスペースが入っているJSデータなどの{}は変更しない
-                            $change_data = '{' . $data . '}';
-                        } else {
-                            $change_data = '';
-                        }
-                        $tag_data = str_replace('{' . $data . '}',
-                            $change_data, $tag_data);
-                    }
-                    $all_tag .= $tag_data;
-                    $tag_data = $original;
-                }
-                $original = '<!-- BEGIN ' . $name . ' -->' . $original;
-                $original .= '<!-- END ' . $name . ' -->';
-                $content = str_replace($original, $all_tag, $content);
-                $content = str_replace('<!-- BEGIN ' . $name . ' -->',
-                    '', $content);
-                $content = str_replace('<!-- END ' . $name . ' -->',
-                    '', $content);
-            } else if (isset($match1[0][0])) {
-                $content = str_replace($match1[0][0], '', $content);
-            }
-        }
-        return $content;
-    }
-
-
-    /**
      * テンプレートの読み込み
      * @param string $tpl テンプレートファイル名
      * @return string 読み込んだコンテンツ
      * @throws \Error
      */
-    private static function open(
-        string $tpl
-    ): string {
+    private static function open(string $tpl): string
+    {
         $content = '';
         $add = preg_match('<\.>', $tpl) ? '' : '.tpl';
         $tpl_folder = (MOBILE_FLAG and !isset($_SESSION['mobile_pc_flag']))
@@ -167,5 +78,136 @@ class View
         }
         fclose($fh);
         return $content;
+    }
+    
+    /**
+     * エレメント部の処理
+     * @param string $content テンプレート
+     * @return string
+     */
+    private static function elementMatch(string $content): string
+    {
+        //部分テンプレートの挿入
+        $element_match = [];
+        preg_match_all('/<!-- ELEMENT (.*?) -->/', $content, $element_match);
+
+        if ($element_match[1]) {
+            $element_match[1] = array_unique($element_match[1]);
+            foreach ($element_match[1] as $ek => $element_name) {
+                $element = self::open($element_name);
+                $content = str_replace($element_match[0][$ek],
+                    $element, $content);
+            }
+            $content = self::elementMatch($content);
+        }
+        return $content;
+    }
+
+    /**
+     * REPLACE部の置換
+     * @param array $disp 表示データ 参照渡し
+     * @param string $content
+     * @return string
+     */
+    private static function replace(array &$disp, string $content): string
+    {
+        if (isset($disp['REPLACE'])) {
+            foreach ($disp['REPLACE'] as $k => $v) {
+                $content = str_replace('{' . $k . '}', $v, $content);
+            }
+            unset($disp['REPLACE']);
+        }
+        return $content;
+    }
+
+    /**
+     * テンプレートから埋め込み部分を検索
+     * @param array|string $disp テンプレートに埋め込むデータ
+     * @param string $content テンプレート
+     * @return void
+     */
+    private static function match($disp, string $content): string
+    {
+        $match = $match1 = $match2 = [];
+        preg_match_all('/<!-- BEGIN (.*?) -->/', $content, $match);
+
+        foreach ($match[1] as $name) {
+            $pattern = '/<!-- BEGIN ' . $name
+                . ' -->(.*)<!-- END ' . $name . ' -->/s';
+            preg_match_all($pattern, $content, $match1);
+            $tag_data = $original = isset($match1[1][0]) ? $match1[1][0] : '';
+
+            preg_match_all('/{(.*?)}/', $original, $match2);
+
+            if (isset($disp[$name])) {
+                $content = self::matchSet(
+                    $content, $original, $disp, $name, $tag_data, $match2);
+            } else if (isset($match1[0][0])) {
+                $content = str_replace($match1[0][0], '', $content);
+            }
+        }
+        return $content;
+    }
+    
+    /**
+     * テンプレートの埋め込み
+     * @param string $content
+     * @param string $original
+     * @param array $disp
+     * @param string $name
+     * @param string $tag_data
+     * @param array $match2
+     * @return string
+     */
+    private static function matchSet(
+        string $content,
+        string $original,
+        array $disp,
+        string $name,
+        string $tag_data,
+        array $match2
+    ): string {
+        $all_tag = '';
+        $num = count($disp[$name]);
+        for ($i = 0; $i < $num; $i ++) {
+            if (isset($disp[$name][$i])) {
+                $tag_data = self::match($disp[$name][$i], $tag_data);
+            }
+            if (isset($match2[1])) {
+                self::matchSetParam($match2[1], $disp[$name][$i], $tag_data);
+            }
+            $all_tag .= $tag_data;
+            $tag_data = $original;
+        }
+        $original = '<!-- BEGIN ' . $name . ' -->'
+            . $original . '<!-- END ' . $name . ' -->';
+        return str_replace(['<!-- BEGIN ' . $name . ' -->',
+            '<!-- END ' . $name . ' -->'], '',
+            str_replace($original, $all_tag, $content));
+    }
+    
+    /**
+     * {}部のパラメーターの挿入
+     * @param array $match 検索結果
+     * @param array|string $disp 表示データ
+     * @param string $tag_data タグデータ 参照渡し
+     * @return void
+     */
+    private static function matchSetParam(
+        array $match,
+        $disp,
+        string &$tag_data
+    ): void {
+        foreach ($match as $data) {
+            if (isset($disp[$data])) {
+                $change_data = $disp[$data];
+            } else if (preg_match('/[ ,:;=]/s', $data)) {
+                // JSデータの{}は変更しない
+                $change_data = '{' . $data . '}';
+            } else {
+                $change_data = '';
+            }
+            $tag_data = str_replace('{' . $data . '}', $change_data, $tag_data);
+        }
     }
 }
