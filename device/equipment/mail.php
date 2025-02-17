fe6358a1763908d1bec25096a2dff0c4330dcfa7
hideshige.sawada <hideshige.sawada@S1062130.local>
2025-02-17 15:38:02 +0900

    メールモジュール変更

--- device/equipment/mail.php
+++ device/equipment/mail.php
@@ -3,7 +3,7 @@
  * メール モジュール
  *
  * @author   Sawada Hideshige
- * @version  1.1.2.0
+ * @version  1.1.3.0
  * @package  device/equipment
  *
  */
@@ -22,6 +22,7 @@ class Mail
      * @param string $subject 件名
      * @param string $body 本文
      * @param string $cc CC
+     * @param array $attachment_paths 添付ファイルパス
      * @return bool
      * @throws \Error
      */
@@ -29,13 +30,15 @@ class Mail
         string $to,
         string $subject,
         string $body,
-        string $cc = ''
+        string $cc = '',
+        array $attachment_paths = []
     ): bool {
+        // メール送信先
         $to2 = self::separate(str_replace(["\n", "\r"], '', $to));
-        
         mb_internal_encoding('utf8');
+        // メール送信元
         $from_name = mb_encode_mimeheader(self::$from_name, 'utf8', 'B');
-        
+        // メールの件名
         $subject2 = '=?utf8?B?' . base64_encode($subject) . '?=';
         $headers = "MIME-Version: 1.0 \n";
         $headers .= sprintf("From: %s<%s> \n", $from_name, self::$from_email);
@@ -43,16 +46,49 @@ class Mail
         if ($cc) {
             $headers .= 'CC: ' . self::separate($cc) . "\n";
         }
-        $headers .= "Content-Type: text/plain;charset=utf8 \n";
+
+        if ($attachment_paths == []) {
+            // テキストのみの場合
+            $headers .= "Content-Type: text/plain;charset=utf8 \n";
+            // メール本文
+            $mail_body = $body;
+        } else {
+            // 添付ファイルありの場合
+            // 境界文字列
+            $boundary = md5(uniqid(time()));
+            $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\n";
+            // メール本文
+            $mail_body = "--$boundary\n";
+            $mail_body .= "Content-Type: text/plain; charset=utf8 \n";
+            $mail_body .= "Content-Transfer-Encoding: 7bit \n\n";
+            $mail_body .= $body . "\n";
+            // 添付ファイル読み込み
+            foreach ($attachment_paths as $path) {
+                if (file_exists($path)) {
+                    $attachment = chunk_split(
+                        base64_encode(file_get_contents($path)));
+                    
+                    // 添付ファイル
+                    $mail_body .= "--$boundary\n";
+                    $mail_body .= "Content-Type: application/octet-stream; name=\"" . basename($path) . "\"\n";
+                    $mail_body .= "Content-Transfer-Encoding: base64 \n";
+                    $mail_body .= "Content-Disposition: attachment; filename=\"" . basename($path) . "\"\n\n";
+                    $mail_body .= $attachment . "\n";
+                } else {
+                    die('ファイルが存在しません');
+                }
+            }
+            $mail_body .= "--$boundary--";
+        }
         $f = sprintf('-f%s', self::$return_email);
 
-        $res = mail($to2, $subject2, $body, $headers, $f);
+        $res = mail($to2, $subject2, $mail_body, $headers, $f);
         if ($res === false) {
             throw new \Error('send mail error');
         }
         return true;
     }
-    
+
     /**
      * メールアドレスを分ける
      * @param string $meta
