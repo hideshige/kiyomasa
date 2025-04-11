7eee861d120495ec4274bdd8d5188a9acc7b1d7c
hideshige.sawada <hideshige.sawada@S1062130.local>
2025-04-11 21:01:48 +0900

    共通部view.php改修

--- device/view.php
+++ device/view.php
@@ -23,7 +23,7 @@
  * <!-- INCLUDE *** -->には指定のテンプレートが挿入される。
  * 
  * @author   Sawada Hideshige
- * @version  1.2.0.0
+ * @version  1.2.2.0
  * @package  device
  * 
  */
@@ -103,12 +103,30 @@ class View
      */
     private static function cleanVariable(string $content): string
     {
-        $match = [];
-        preg_match_all('/{(.*?)}/', $content, $match);
-        if (isset($match[1]) && $match[1]) {
-            foreach ($match[1] as $k => $v) {
-                if (!self::checkCode($v)) {
-                    $content = str_replace($match[0][$k], '', $content);
+        $script = [];
+        $match1 = $match2 = $match3 = [];
+        
+        // スクリプト部の取得
+        $pattern = '/<script(.*?)<\/script>/s';
+        preg_match_all($pattern, $content, $match1);
+        if (isset($match1[1]) && $match1[1]) {
+            foreach ($match1[1] as $v) {
+                preg_match_all('/{(.*?)}/', $v, $match2);
+                if (isset($match2[1]) && $match2[1]) {
+                    foreach ($match2[1] as $v) {
+                        $script[$v] = 1;
+                    }
+                }
+            }
+        }
+        
+        // 全体の取得
+        preg_match_all('/{(.*?)}/', $content, $match3);
+        if (isset($match3[1]) && $match3[1]) {
+            foreach ($match3[1] as $k => $v) {
+                // スクリプトを除外
+                if (!self::checkCode($v) && !isset($script[$v])) {
+                    $content = str_replace($match3[0][$k], '', $content);
                 }
             }
         }
@@ -298,6 +316,6 @@ class View
      */
     private static function checkCode(string $string): bool
     {
-        return (preg_match("/[ ,'\";=]/s", $string) || $string === '');
+        return (preg_match("/[ ,'\".;=]/s", $string) || $string === '');
     }
 }
